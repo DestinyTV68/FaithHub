f8008b8e73e76509d29532a4e10363efe6d01865
Don Prof <48210324+Donprof29@users.noreply.github.com>
2025-08-15 19:55:21 +0000

    Quartz sync: Aug 15, 2025, 7:55 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
 interface Options {
@@ -9,16 +8,16 @@ interface Options {
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          Contact us at{" "}
+          <a href="https://t.me/DestinyTV" target="_blank" rel="noopener">
+            DestinyTV
+          </a>
         </p>
         <ul>
-          {Object.entries(links).map(([text, link]) => (
+          {Object.entries(opts?.links ?? {}).map(([text, link]) => (
             <li>
               <a href={link}>{text}</a>
             </li>
