5185e197444f05262312eecf5d41ade073bd42d8
Don Prof <48210324+Donprof29@users.noreply.github.com>
2025-08-15 18:14:48 +0000

    updated sitetitle

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "FaithHub",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
